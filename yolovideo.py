853b6448f5236339186f76d1536b0451d1ee05b8
LizzieT <49140813+LizzieTang@users.noreply.github.com>
2019-06-16 21:07:47 +0800

    set the bbox to be stable

--- yolovideo.py
+++ yolovideo.py
@@ -130,10 +130,10 @@ def write(x, img):
     # color = random.choice(colors)
     color = COLORS[cls]
     cv2.rectangle(img, c1, c2,color, 1)
-    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
-    c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
+    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.8 , 1)[0]
+    c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 6
     cv2.rectangle(img, c1, c2,color, -1)
-    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
+    cv2.putText(img, label, (c1[0], c1[1] + t_size[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.8, [225,255,255], 1);
     return img
 
 def arg_parse():
